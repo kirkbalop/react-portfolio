c76c092b9dd8efb9d78d1c5b9ac346054961f94b
Kirk <kirkbalop@gmail.com>
2021-06-03 09:26:21 -0700

    update copy

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
 
 function About() {
   return (
-    <section className='my-5'>
+    <section className="my-5">
       <h1 id="about">About me</h1>
       <p>
-          My name is Kirk Balopoulos, and I am a full stack Web Developer from San Francisco, California. 
-          I recieved my education with regard to Web Development from the University of California, Berkeley Extension.
+        My name is Kirk Balopoulos, and I am a full stack Web Developer from San
+        Francisco, California. I recently earned certification in full stack
+        development from the bootcamp at the University of California, Berkeley.
+        I've developed skills in a host of front and back end technologies with
+        a focus on responsive, mobile-first design. I'm an innovative, outside
+        of the box thinker with excellent problem-solving skills and work ethic.
+        I pride myself in identifying and finding solutions to problems through
+        independent research whenever possible.
+        <br></br>
+        In addition to my technical skillset, my 15+ years of working with the
+        public in both a customer service setting as well as in their homes has
+        allowed me to finely hone my communication skills both with customers
+        and in a team setting. I'm very good at both giving and taking direction
+        and understand the importance of thoughtful and constructive
+        communication when working as a unit.
       </p>
     </section>
   );
