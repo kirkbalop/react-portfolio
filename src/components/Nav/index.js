f6d3527c506ba7f945a471151d10b44a3c604b27
Kirk <kirkbalop@gmail.com>
2021-06-03 09:47:15 -0700

    remove contact from nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 
 function Nav(props) {
-    const { currentTab, setCurrentTab } = props;
-    return(
-        <nav>
-            <ul className="flex-row mobile-view">
-                <li className={currentTab === 'about' ? 'mx-2 navActive' : 'mx-2'}>
-                    <span onClick={() => setCurrentTab("about")}>About Me</span>
-                </li>
-                <li className={currentTab === 'portfolio' ? 'mx-2 navActive' : 'mx-2'}>
-                    <span onClick={() => setCurrentTab("portfolio")}>Portfolio</span>
-                </li>
-                <li className={currentTab === 'resume' ? 'mx-2 navActive' : 'mx-2'}>
-                    <span onClick={() => setCurrentTab("resume")}>Resume</span>
-                </li>
-                <li className={currentTab === 'contact' ? 'mx-2 navActive' : 'mx-2'}>
+  const { currentTab, setCurrentTab } = props;
+  return (
+    <nav>
+      <ul className="flex-row mobile-view">
+        <li className={currentTab === "about" ? "mx-2 navActive" : "mx-2"}>
+          <span onClick={() => setCurrentTab("about")}>About Me</span>
+        </li>
+        <li className={currentTab === "portfolio" ? "mx-2 navActive" : "mx-2"}>
+          <span onClick={() => setCurrentTab("portfolio")}>Portfolio</span>
+        </li>
+        <li className={currentTab === "resume" ? "mx-2 navActive" : "mx-2"}>
+          <span onClick={() => setCurrentTab("resume")}>Resume</span>
+        </li>
+        {/* <li className={currentTab === 'contact' ? 'mx-2 navActive' : 'mx-2'}>
                     <span onClick={() => setCurrentTab("contact")}>Contact</span>
-                </li>
-            </ul>
-        </nav>
-    );
+                </li> */}
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
