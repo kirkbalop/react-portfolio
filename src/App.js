2a57f672bf0a179f06ca08012d38cc1aeb5d3162
Kirk <kirkbalop@gmail.com>
2021-06-03 09:39:43 -0700

    disable contact until POST route written

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
-import {React, useState} from 'react';
-import './App.css';
-import About from './components/About';
-import Header from './components/Header';
-import Portfolio from './components/Portfolio';
-import Resume from './components/Resume';
-import Contact from './components/Contact';
-import Footer from './components/Footer'
+import { React, useState } from "react";
+import "./App.css";
+import About from "./components/About";
+import Header from "./components/Header";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
-  const [currentTab, setCurrentTab] = useState('about');
+  const [currentTab, setCurrentTab] = useState("about");
 
   // check selected tab and render accordingly
   const renderTab = () => {
     switch (currentTab) {
-      case 'about':
+      case "about":
         return <About />;
-      case 'portfolio':
-        return <Portfolio />
-      case 'resume':
-        return <Resume />
-      case 'contact':
-        return <Contact />
+      case "portfolio":
+        return <Portfolio />;
+      case "resume":
+        return <Resume />;
+      // case 'contact':
+      //   return <Contact />
       default:
-        return <About />
+        return <About />;
     }
-  }
+  };
 
   return (
-		<div>
-			<div className="mobile-header">
-				<Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
-			</div>
-			<div>
-				<main>{renderTab()}</main>
-			</div>
-			<div>
-				<Footer></Footer>
-			</div>
-		</div>
-	);
+    <div>
+      <div className="mobile-header">
+        <Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
+      </div>
+      <div>
+        <main>{renderTab()}</main>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
