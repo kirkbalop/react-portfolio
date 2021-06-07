9083102ebca2c43bf4628e44a97a268cfa43ddd6
Kirk <kirkbalop@gmail.com>
2021-06-07 13:39:50 -0700

    fixed LinkedIn link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,37 +1,37 @@
 import React from "react";
 
 function Footer() {
-	return (
-		<footer className="footer">
-			<div>
-				<a
-					href="https://github.com/KirkBalop"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img
-						src={process.env.PUBLIC_URL + `/assets/logos/github-logo.png`}
-						alt="Github"
-						className="logo"
-					></img>
-				</a>
-			</div>
-			<div>
-				<a
-					href="www.linkedin.com/in/kirk-balopoulos-255407104
+  return (
+    <footer className="footer">
+      <div>
+        <a
+          href="https://github.com/KirkBalop"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={process.env.PUBLIC_URL + `/assets/logos/github-logo.png`}
+            alt="Github"
+            className="logo"
+          ></img>
+        </a>
+      </div>
+      <div>
+        <a
+          href="http://www.linkedin.com/in/kirk-balopoulos-255407104
                     "
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img
-						src={process.env.PUBLIC_URL + `/assets/logos/linkedin-logo.png`}
-						alt="LinkedIn"
-						className="logo"
-					></img>
-				</a>
-			</div>
-		</footer>
-	);
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={process.env.PUBLIC_URL + `/assets/logos/linkedin-logo.png`}
+            alt="LinkedIn"
+            className="logo"
+          ></img>
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
